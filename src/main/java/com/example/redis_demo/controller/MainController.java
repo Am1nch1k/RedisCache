bbd732c8d7a187c257949fdd5b476ae22721fbdd
Amin Sadigov <a1234@Amin-Sadigov.local>
2025-08-20 10:29:08 +0700

    Redis ready in start

---
+++ src/main/java/com/example/redis_demo/controller/MainController.java
@@ -0,0 +1,33 @@
+package com.example.redis_demo.controller;
+
+import com.example.redis_demo.entity.User;
+import com.example.redis_demo.repository.UserRepository;
+import com.example.redis_demo.service.UserService;
+import lombok.experimental.Accessors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class MainController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody User user){
+        return new ResponseEntity<>(userService.save(user), HttpStatusCode.valueOf(201));
+    }
+    @GetMapping
+    public ResponseEntity<User> getById(@RequestParam Integer id){
+        User user = userService.getById(id);
+        if(user == null){
+            return new ResponseEntity<>(HttpStatusCode.valueOf(400));
+        }
+        return new ResponseEntity<>(user, HttpStatusCode.valueOf(200));
+    }
+
+}
